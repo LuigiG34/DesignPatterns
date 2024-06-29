3969afcf09193db8fe91efc589b77bc51aec894d
Luigi Gandemer <luigigandemer6@gmail.com>
2024-06-29 12:44:42 +0200

    Structural Patterns

--- Structural/Decorator/Decorator.php
+++ Structural/Decorator/Decorator.php
@@ -2,6 +2,144 @@
 
 namespace LuigiG34\Structural\Decorator;
 
+interface InputFormat
+{
+    public function formatText(string $text): string;
+}
+
+class TextInput implements InputFormat
+{
+    public function formatText(string $text): string
+    {
+        return $text;
+    }
+}
+
+class TextFormat implements InputFormat
+{
+    /**
+     * @var InputFormat
+     */
+    protected $inputFormat;
+
+    public function __construct(InputFormat $inputFormat)
+    {
+        $this->inputFormat = $inputFormat;
+    }
+
+    public function formatText(string $text): string
+    {
+        return $this->inputFormat->formatText($text);
+    }
+}
+
+class PlainTextFilter extends TextFormat
+{
+    public function formatText(string $text): string
+    {
+        $text = parent::formatText($text);
+        return strip_tags($text);
+    }
+}
+
+class DangerousHTMLTagsFilter extends TextFormat
+{
+    private $dangerousTagPatterns = [
+        "|<script.*?>([\s\S]*)?</script>|i",
+    ];
+
+    private $dangerousAttributes = [
+        "onclick", "onkeypress",
+    ];
+
+
+    public function formatText(string $text): string
+    {
+        $text = parent::formatText($text);
+
+        foreach ($this->dangerousTagPatterns as $pattern) {
+            $text = preg_replace($pattern, '', $text);
+        }
+
+        foreach ($this->dangerousAttributes as $attribute) {
+            $text = preg_replace_callback('|<(.*?)>|', function ($matches) use ($attribute) {
+                $result = preg_replace("|$attribute=|i", '', $matches[1]);
+                return "<" . $result . ">";
+            }, $text);
+        }
+
+        return $text;
+    }
+}
+
+class MarkdownFormat extends TextFormat
+{
+    public function formatText(string $text): string
+    {
+        $text = parent::formatText($text);
+        $chunks = preg_split('|\n\n|', $text);
+        foreach ($chunks as &$chunk) {
+            if (preg_match('|^#+|', $chunk)) {
+                $chunk = preg_replace_callback('|^(#+)(.*?)$|', function ($matches) {
+                    $h = strlen($matches[1]);
+                    return "<h$h>" . trim($matches[2]) . "</h$h>";
+                }, $chunk);
+            }
+            else {
+                $chunk = "<p>$chunk</p>";
+            }
+        }
+        $text = implode("\n\n", $chunks);
+
+        $text = preg_replace("|__(.*?)__|", '<strong>$1</strong>', $text);
+        $text = preg_replace("|\*\*(.*?)\*\*|", '<strong>$1</strong>', $text);
+        $text = preg_replace("|_(.*?)_|", '<em>$1</em>', $text);
+        $text = preg_replace("|\*(.*?)\*|", '<em>$1</em>', $text);
+
+        return $text;
+    }
+}
+
 /**
  * @example Example d'utilisation du Pattern Decorator
  */
+$dangerousComment = <<<HERE
+Hello! Nice blog post!
+Please visit my <a href='http://www.iwillhackyou.com'>homepage</a>.
+<script src="http://www.iwillhackyou.com/script.js">
+  performXSSAttack();
+</script>
+HERE;
+
+$naiveInput = new TextInput();
+echo "Website renders comments without filtering (unsafe):\n";
+echo $naiveInput->formatText($dangerousComment);
+echo "\n\n\n";
+
+$filteredInput = new PlainTextFilter($naiveInput);
+echo "Website renders comments after stripping all tags (safe):\n";
+echo $filteredInput->formatText($dangerousComment);
+echo "\n\n\n";
+
+$dangerousForumPost = <<<HERE
+# Welcome
+
+This is my first post on this **gorgeous** forum.
+
+<script src="http://www.iwillhackyou.com/script.js">
+  performXSSAttack();
+</script>
+HERE;
+
+$naiveInput = new TextInput();
+echo "Website renders a forum post without filtering and formatting (unsafe, ugly):\n";
+echo $naiveInput->formatText($dangerousForumPost);
+echo "\n\n\n";
+
+$text = new TextInput();
+$markdown = new MarkdownFormat($text);
+$filteredInput = new DangerousHTMLTagsFilter($markdown);
+echo "Website renders a forum post after translating markdown markup" .
+    " and filtering some dangerous HTML tags and attributes (safe, pretty):\n";
+echo $filteredInput->formatText($dangerousForumPost);
+echo "\n\n\n";
